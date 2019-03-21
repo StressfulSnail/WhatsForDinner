7484f6882ec140268255be272d04636203a6d6d9
StressfulSnail <stressfulsnail@gmail.com>
2019-03-20 20:32:14 -0500

    setup knex connection to db

--- server/server.js
+++ server/server.js
@@ -2,6 +2,17 @@ require('dotenv').config(); // Load in environment variables from .env
 const express = require('express');
 const app = express();
 
+// Initializing knex
+const knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: 'whats_for_dinner',
+    }
+});
+
 app.get('/', (req, res) => res.send('Hello!'));
 
 app.listen(process.env.SERVER_PORT, () => console.log(`Server listening on port ${process.env.SERVER_PORT}!`));
