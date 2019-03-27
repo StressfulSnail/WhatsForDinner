14ac3dd772713231e77c0574bfe978f9b39e0739
StressfulSnail <stressfulsnail@gmail.com>
2019-03-27 14:54:13 -0500

    updated timestamps to be populated automatically on inserts/updates

--- db/migrations/patch-0.sql
+++ db/migrations/patch-0.sql
@@ -2,8 +2,8 @@ CREATE TABLE `account_invitation` (
   `invitation_id` INT NOT NULL AUTO_INCREMENT,
   `account_id` INT NOT NULL,
   `invitation_key` VARCHAR(255) NOT NULL,
-  `created_on` TIMESTAMP NOT NULL,
-  `modified_on` TIMESTAMP NOT NULL,
+  `created_on` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `modified_on` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`invitation_id`),
   UNIQUE KEY unique_invitation_key (`invitation_key`),
   FOREIGN KEY (`account_id`) REFERENCES `account`(`account_id`)
