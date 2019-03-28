10b9284b443a48d2b1d6a9a16dfbb1b3949b5175
Duncan Henry <bp8680ta@metrostate.edu>
2019-03-28 17:00:07 -0500

    Commit test

---
+++ server/model/Ingredient.js
@@ -0,0 +1,18 @@
+//Ingredient class -- Duncan Henry
+//Unsure as to how this relates to IngredientCount and Recipe.
+
+class Ingredient {
+
+    constructor(){
+        this.name = null;
+    }
+
+
+    getName(){
+        return this.name;
+    }
+
+    setName(newName){
+        this.name = newName;
+    }
+}
