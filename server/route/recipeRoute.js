1ccec22e97326ca813363dec9e4b1d1bab7c1cab
Duncan Henry <bp8680ta@metrostate.edu>
2019-04-01 13:46:26 -0500

    Did a first pass on recipeService
    Feedback greatly appreciated.

--- server/route/recipeRoute.js
+++ server/route/recipeRoute.js
@@ -1,1 +1,71 @@
-//To be filled in later
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+const accountController = require('../controller/accountController');
+
+/**
+ * GET /api/recipe/
+ * Get account details
+ * Returns {
+ *      name: string
+ *      imageURL: string
+ *      ingredientList: Array(Ingredient)
+ *      prepInstructions: string
+ *      prepTime: int
+ *      cookTime: int
+ *      caloricEstimate: int
+ *      tasteRating: int
+ *      difficultyRating: int
+ *      tags: array(Tag);
+ * }
+ */
+router.get('/', passport.authenticate('jwt', { session: false }), accountController.getAccountById);
+
+/**
+ * POST /api/account/
+ * Create a new account
+ * Body {
+ *      email: string
+ *      username: string
+ *      password: string
+ *      firstName: string
+ *      middleName: string
+ *      lastName: string
+ * }
+ */
+router.post('/', accountController.createAccount);
+
+/**
+ * POST /api/account/confirm/:invitationId
+ * Confirm account with invitation key
+ */
+router.post('/confirm/:invitationId', accountController.confirmAccount);
+
+/**
+ * POST /api/account/validate
+ * Validate user
+ * Body {
+ *     username: string
+ *     password: string
+ * }
+ */
+router.post('/validate', accountController.validateUser);
+
+/**
+ * DELETE /api/account/delete
+ * Delete Account
+ *
+ */
+router.delete('/delete', passport.authenticate ('jwt', {session: false }), accountController.deleteAccount);
+
+/**
+ * POST /api/account/edit
+ * Edit Account
+ *
+ */
+router.post('/edit', passport.authenticate ('jwt', {session: false }), accountController.editAccount);
+
+
+
+module.exports = router;
