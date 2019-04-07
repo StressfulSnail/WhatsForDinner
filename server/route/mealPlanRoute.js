b611ec461fe3b9e15c1c3bc0cda0f1bac11229e9
StressfulSnail <stressfulsnail@gmail.com>
2019-04-07 11:25:07 -0500

    #43 planned routes

--- server/route/mealPlanRoute.js
+++ server/route/mealPlanRoute.js
@@ -28,4 +28,99 @@ const mealPlanController = require('../controller/mealPlanController');
  */
 router.post('/', passport.authenticate('jwt', { session: false }), mealPlanController.createMealPlan);
 
+/**
+ * GET /api/mealplan/
+ * Get all meal plans for account
+ * Response [
+ *   {
+ *      id: number
+ *      name: string
+ *      startDate: Date
+ *      endDate: Date
+ *   }
+ * ]
+ */
+router.get('/');
+
+/**
+ * PUT /api/mealplan/:mealPlanId
+ * Update meal plan with given id
+ * Body {
+ *    id: number
+ *    name: string
+ *    startDate: Date
+ *    endDate: Date
+ * }
+ */
+router.put('/:mealPlanId');
+
+/**
+ * DELETE /api/mealplan/:mealPlanId
+ * Delete meal plan with given id, will delete any children meals
+ */
+router.delete('/:mealPlanId');
+
+/**
+ * GET /api/mealplan/:mealPlanId/meals
+ * Get all meals for a meal plan
+ * Response [
+ *  {
+ *    id: number
+ *    dateTime: Date
+ *    servingsRequired: number
+ *    note: string
+ *    recipes: [
+ *        {
+ *          id: int
+ *          name: string
+ *        }
+ *    ]
+ *  }
+ *]
+ */
+router.get('/:mealPlanId/meals');
+
+/**
+ * POST /api/mealplan/:mealPlanId/meals
+ * Add new meal into meal plan
+ * Body {
+ *    dateTime: Date
+ *    servingsRequired: number
+ *    note: string
+ *    recipes: [
+ *        {
+ *          id: int
+ *          name?: string
+ *        }
+ *    ]
+ *  }
+ */
+router.post('/:mealPlanId/meals');
+
+/**
+ * PUT /api/mealplan/:mealPlanId/meals/:mealId
+ * Update meal with given mealId
+ * Body {
+ *    id: number
+ *    dateTime: Date
+ *    servingsRequired: number
+ *    note: string
+ *    recipes: [
+ *        {
+ *          id: int
+ *          name?: string
+ *        }
+ *    ]
+ *  }
+ */
+router.put('/:mealPlanId/meals/:mealId');
+
+/**
+ * DELETE /api/mealplan/:mealPlanId/meals/:mealId
+ * Delete meal with given mealId
+ */
+router.delete('/:mealPlanId/meals/:mealId');
+
+
+
 module.exports = router;
