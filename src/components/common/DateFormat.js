1147252536da1c34705734fff89457e81995d8bf
StressfulSnail <stressfulsnail@gmail.com>
2019-04-17 14:30:40 -0500

    #49 annoying timezone issues

--- src/components/common/DateFormat.js
+++ src/components/common/DateFormat.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+export const dateFormat = (value) => {
+    const date = value instanceof Date ? value : new Date(value);
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+};
+
+/**
+ * changes yyyy-mm-dd to a Date object with correct time zone
+ * @param inputText - yyyy-mm-dd
+ * @returns {Date}
+ */
+export const inputDateToDateObject = (inputText) => {
+    const offset = new Date().getTimezoneOffset() * 60000;
+    return new Date(Date.parse(inputText) + offset);
+};
+
 const DateFormat = function (props) {
     const { value } = props;
-    const date = value instanceof Date ? value : new Date(value);
-    return <>{`${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`}</>;
+    return <>{dateFormat(value)}</>;
 };
 
 DateFormat.propTypes = {
