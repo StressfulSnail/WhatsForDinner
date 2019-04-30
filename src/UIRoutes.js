7e2abb013f7f13432b7b13bc3dcd31fdce6e703b
Colin Quinn <cquinn540@gmail.com>
2019-04-30 09:36:24 -0500

    Made a Navigation Bar.  Can logout and navigate to each view except public recieps which doesn't exist yet.

--- src/UIRoutes.js
+++ src/UIRoutes.js
@@ -8,21 +8,31 @@ import HomePage from './views/public/HomePage.js';
 import MealPlansPage from './views/public/MealPlansPage';
 import MealPlanPage from './views/public/MealPlanPage';
 import RecipePage from './views/public/MyCookbook';
+import AccountPage from './views/public/AccountPage';
 import LoadingIndicator from "./components/common/LoadingIndicator";
 import {connect} from "react-redux";
+
+const LANDING = "/";
+const HOME = "/home";
+const ACCOUNT = "/account";
+const CREATE_ACCOUNT = "/create-account";
+const RECOVER_ACCOUNT = "/recover-account";
+const MY_COOKBOOK = "/my-cookbook";
+const MEAL_PLANS = "/meal-plans";
 
 class UIRoutes extends Component {
     render() {
         return (
             <div>
                 <LoadingIndicator isLoading={this.props.isLoading}/>
-                <Route exact path="/" component={LandingPage} />
-                <Route path="/create-account" component={CreateAccountPage} />
-                <Route path="/recover-account" component={AccountRecoveryPage} />
-                <Route path="/home" component={HomePage} />
-                <Route path="/my-cookbook" component={RecipePage} />
-                <Route exact path="/meal-plans" component={MealPlansPage} />
-                <Route exact path="/meal-plans/:id" component={MealPlanPage} />
+                <Route exact path={LANDING} component={LandingPage} />
+                <Route path={CREATE_ACCOUNT} component={CreateAccountPage} />
+                <Route path={RECOVER_ACCOUNT} component={AccountRecoveryPage} />
+                <Route path={HOME} component={HomePage} />
+                <Route path={ACCOUNT} component={AccountPage} />
+                <Route path={MY_COOKBOOK} component={RecipePage} />
+                <Route exact path={MEAL_PLANS} component={MealPlansPage} />
+                <Route exact path={`${MEAL_PLANS}/:id`} component={MealPlanPage} />
             </div>
         )
     }
@@ -33,5 +43,15 @@ const mapStateToProps = (state) => {
         isLoading: state.main.isLoading,
     }
 };
+
+export {
+    LANDING,
+    HOME,
+    ACCOUNT,
+    CREATE_ACCOUNT,
+    RECOVER_ACCOUNT,
+    MY_COOKBOOK,
+    MEAL_PLANS
+}
 
 export default connect(mapStateToProps)(UIRoutes);
